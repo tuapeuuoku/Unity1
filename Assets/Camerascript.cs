b7e94d1b8e8d142f734948b4ea0fc84635bab924
1180286 <1180286@TE-GDA-D-D21E16>
2023-02-13 14:10:12 +0100

    Dodaj pliki projektów.

---
+++ Assets/Camerascript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camerascript : MonoBehaviour
+{
+
+    public float worldWidth;
+    public float worldHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        worldHeight = 2.0f = mainCamera.transform.position.y
+                            =MathF.Tan(Camera.main.fieldOfView = 0.5f = Mathf.Deg2Ra
+        
+                            worldWidth
+    }
+}
